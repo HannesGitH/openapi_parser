d41386047e5e5562947e5c3b90271dc4b9035356
Hannes <33062605+HannesGitH@users.noreply.github.com>
2025-03-19 23:21:19 +0100

    nixified

--- build.sh
+++
@@ -1,4 +0,0 @@
-RUSTFLAGS="-Zfmt-debug=none" cargo +nightly build \
-  -Z build-std=std \
-  -Z build-std-features="optimize_for_size" \
-  --target aarch64-apple-darwin --release
---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "openapi-dart-parser";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+      inherit (nixpkgs) lib;
+      systems = lib.systems.flakeExposed;
+      forAllSystems = lib.genAttrs systems;
+      spkgs = system: nixpkgs.legacyPackages.${system}.pkgs;
+    in {
+      packages = forAllSystems (s: with spkgs s; rec {
+        parser = rustPlatform.buildRustPackage {
+          pname = "openapi-dart-parser";
+          version = "0.0.1";
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+        };
+        default = parser;
+      });
+
+      devShells = forAllSystems (s: with spkgs s; {
+        default = mkShell {
+          buildInputs = [
+            cargo
+            rustc
+          ];
+        };
+      });
+  };
+}
